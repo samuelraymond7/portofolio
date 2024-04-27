4a0a796ebbcf38606f5b429e21f17543b285699d
Samuel Raymond <samuelraymond711@gmail.com>
2024-04-27 10:26:04 +0700

    Button component V 1.0

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 
-const Button = ({ children, arrow = true }) => (
+const Button = ({ children, arrow = true, tipe = "button", className }) => (
   <button
-    type="button"
-    className="group flex h-10 items-center justify-center rounded-md px-4 py-2 text-[17px] font-medium transition-colors outline-none bg-transparent hover:bg-slate-800 hover:text-slate-50 focus:bg-slate-900 "
+    type={tipe === "button" ? "button" : "submit"}
+    className={`flex h-10 items-center justify-center rounded-md px-4 py-2 text-[17px] font-medium transition-colors outline-none bg-transparent hover:bg-slate-800 hover:text-slate-50 focus:bg-slate-900 ${className} `}
   >
     {children}
     {arrow && <MdOutlineArrowDropDown className=" ml-1" />}
