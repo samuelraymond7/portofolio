af9a6ec95ae9b42a423cc259b6e0250045a4977a
Samuel Raymond <samuelraymond711@gmail.com>
2024-04-27 10:43:00 +0700

    Button component V 1.1

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -7,7 +7,9 @@ const Button = ({ children, arrow = true, tipe = "button", className }) => (
     className={`flex h-10 items-center justify-center rounded-md px-4 py-2 text-[17px] font-medium transition-colors outline-none bg-transparent hover:bg-slate-800 hover:text-slate-50 focus:bg-slate-900 ${className} `}
   >
     {children}
-    {arrow && <MdOutlineArrowDropDown className=" ml-1" />}
+    {arrow && (
+      <MdOutlineArrowDropDown className=" ml-1 group-hover:rotate-180 transition duration-200" />
+    )}
   </button>
 );
 
