6417ad6dd7963a50b1b76431c2cc5f14a2ecfbdb
Samuel Raymond <samuelraymond711@gmail.com>
2024-04-27 19:57:09 +0700

    add on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
@@ -11,81 +11,94 @@ import {
   NavigationMenuContent,
 } from "./ui/Menubar";
 
-const Navbar = () => (
-  <header className=" sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-transparent">
-    <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
-      {/* Logo dan Nama */}
-      <Link
-        href="/"
-        className="flex items-center gap-2"
-        onClick={() => window.scrollTo(0, 0)}
-      >
-        <Image
-          src="./icon.svg"
-          alt="logo"
-          className="w-9 h-9 object-contain"
-          width={10}
-          height={10}
-        />
-        <p className=" text-white text-[18px] font-bold flex">
-          Samuel Raymond &nbsp;
-          <span className=" lg:block hidden">| Web Developer</span>
-        </p>
-      </Link>
+const Navbar = () => {
+  const [navScroll, setNavScroll] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 76) {
+      setNavScroll(true);
+    } else {
+      setNavScroll(false);
+    }
+  };
+  window.addEventListener("scroll", changeColor);
+  return (
+    <header
+      className={`sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 ${navScroll ? "bg-black" : "bg-transparent"}`}
+    >
+      <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logo dan Nama */}
+        <Link
+          href="/"
+          className="flex items-center gap-2"
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          <Image
+            src="./icon.svg"
+            alt="logo"
+            className="w-9 h-9 object-contain"
+            width={10}
+            height={10}
+          />
+          <p className=" text-white text-[18px] font-bold flex">
+            Samuel Raymond &nbsp;
+            <span className=" lg:block hidden">| Web Developer</span>
+          </p>
+        </Link>
 
-      {/* NavBar */}
-      <nav>
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList
-            trigger="Home"
-            className="peer-hover/Home:flex peer-focus/Home:flex"
-          >
-            {navLinks.map((nav) => (
-              <NavigationMenuContent
-                key={nav.id}
-                id={nav.id}
-                title={nav.title}
-                link={nav.link}
-              >
-                {nav.content}
-              </NavigationMenuContent>
-            ))}
-          </NavigationMenuList>
+        {/* NavBar */}
+        <nav>
+          <NavigationMenu className="hidden md:flex">
+            <NavigationMenuList
+              trigger="Home"
+              className="peer-hover/Home:flex peer-focus/Home:flex"
+            >
+              {navLinks.map((nav) => (
+                <NavigationMenuContent
+                  key={nav.id}
+                  id={nav.id}
+                  title={nav.title}
+                  link={nav.link}
+                >
+                  {nav.content}
+                </NavigationMenuContent>
+              ))}
+            </NavigationMenuList>
 
-          <NavigationMenuList
-            trigger="Games"
-            className="peer-hover/Games:flex peer-focus/Games:flex"
-          >
-            No games available yet
-          </NavigationMenuList>
+            <NavigationMenuList
+              trigger="Games"
+              className="peer-hover/Games:flex peer-focus/Games:flex"
+            >
+              No games available yet
+            </NavigationMenuList>
 
-          <Link
-            className="flex h-10 items-center justify-center rounded-md px-4 py-2 text-[17px] font-medium transition-colors outline-none bg-transparent hover:bg-slate-800 hover:text-slate-50"
-            href="https://www.linkedin.com/in/samuel-raymond-pranowo/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            LinkedIn
-          </Link>
-        </NavigationMenu>
+            <Link
+              className="flex h-10 items-center justify-center rounded-md px-4 py-2 text-[17px] font-medium transition-colors outline-none bg-transparent hover:bg-slate-800 hover:text-slate-50"
+              href="https://www.linkedin.com/in/samuel-raymond-pranowo/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </Link>
+          </NavigationMenu>
 
-        <div className=" flex md:hidden fixed top-0 right-0 hover:w-44 h-full hover:bg-black hover:bg-opacity-60 transition-all duration-200 ease-out group flex-col">
-          <ul>
-            <li className="flex justify-start items-center py-4 px-2 group-hover:bg-slate-800 group-hover:bg-opacity-60">
-              <MdKeyboardDoubleArrowLeft
-                className="group-hover:rotate-180 transition-all duration-200 ease-in"
-                size={40}
-              />
-              <p className="hidden opacity-0 group-hover:block group-hover:opacity-100 transition-all ease-in duration-200 mx-auto">
-                MENU
-              </p>
-            </li>
-            <li className=" group-hover:block hidden">tes</li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </header>
-);
+          <div className=" flex md:hidden fixed top-0 right-0 hover:w-44 h-full hover:bg-black hover:bg-opacity-60 transition-all duration-200 ease-out group flex-col">
+            <ul>
+              <li className="flex justify-start items-center py-4 px-2 group-hover:bg-slate-800 group-hover:bg-opacity-60">
+                <MdKeyboardDoubleArrowLeft
+                  className="group-hover:rotate-180 transition-all duration-200 ease-in"
+                  size={40}
+                />
+                <p className="hidden opacity-0 group-hover:block group-hover:opacity-100 transition-all ease-in duration-200 mx-auto">
+                  MENU
+                </p>
+              </li>
+              <li className=" group-hover:block hidden">tes</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
