acd949bf4d61890c3f565631860f98549d41ac23
Samuel Raymond <samuelraymond711@gmail.com>
2024-05-01 01:17:23 +0700

    hero section V 1.2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 
 const Hero = () => (
   <section className=" bg-hero bg-cover bg-no-repeat bg-center w-full h-screen sm:px-16 px-6">
-    <div className="pt-[160px] pb-[220px] md:py-[80px] max-w-7xl mx-auto h-full flex flex-col justify-between relative">
+    <div className="pt-[160px] sm:pt-[120px] max-w-7xl mx-auto h-full flex flex-col gap-20 relative">
       <div className="flex flex-row items-start gap-3">
         <div className=" flex flex-col justify-center items-center mt-3 ml-2">
           <div className=" w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className=" w-1 sm:h-80 h-40 violet-gradient" />
+          <div className=" w-1 lg:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
@@ -22,9 +22,9 @@ const Hero = () => (
         </div>
       </div>
 
-      <div className=" w-full flex justify-center relative">
-        <div className="absolute z-10 p-2 bg-slate-400 opacity-90 rounded-full left-5 top-[65px]">
-          <RiArrowLeftSLine size={15} />
+      <div className=" w-full flex justify-center relative lg:absolute lg:top-80 lg:justify-end">
+        <div className="absolute z-10 p-2 bg-slate-400 opacity-90 rounded-full left-5 top-[65px] sm:left-7 sm:top-[95px] md:left-9 md:top-[120px]">
+          <RiArrowLeftSLine className=" text-[15px] sm:text-[20px] md:text-[25px]" />
         </div>
 
         <Image
@@ -32,11 +32,11 @@ const Hero = () => (
           alt="Hero Logo"
           width={500}
           height={500}
-          className=" object-contain w-4/5 z-0"
+          className=" object-contain w-4/5 z-0 max-w-[700px] border-slate-500 border-2 rounded-lg"
         />
 
-        <div className="absolute z-10 p-2 bg-slate-400 opacity-90 rounded-full right-5 top-[65px]">
-          <RiArrowRightSLine size={15} />
+        <div className="absolute z-10 p-2 bg-slate-400 opacity-90 rounded-full right-5 top-[65px] sm:right-7 sm:top-[95px] md:right-9 md:top-[120px]">
+          <RiArrowRightSLine className=" text-[15px] sm:text-[20px] md:text-[25px]" />
         </div>
       </div>
 
