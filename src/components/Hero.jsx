22ac144b8ab2890bb6e92907920d3d0b070d12e0
Samuel Raymond <samuelraymond711@gmail.com>
2024-04-30 20:52:14 +0700

    Hero section V 1.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Hero = () => (
-  <section className=" bg-hero bg-cover bg-no-repeat bg-center">
-    hero
-    <div>coba</div>
+  <section className=" bg-hero bg-cover bg-no-repeat bg-center relative w-full h-screen ">
+    <div className=" 2xl:px-0 sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
+      <div className=" flex flex-col justify-center items-center mt-5">
+        <div className=" w-5 h-5 rounded-full bg-[#915eff]" />
+        <div className=" w-1 sm:h-80 h-40 violet-gradient" />
+      </div>
+
+      <div>
+        <h1>
+          Hi, I&apos;m <span className=" text-[#915eff]">Samuel</span>
+        </h1>
+        <p>
+          I develop user interfaces <br className=" sm:block hidden" />
+          and web applications
+        </p>
+      </div>
+    </div>
   </section>
 );
 
