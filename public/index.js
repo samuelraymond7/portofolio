0a8221b625491d86706521e2e8b1640c997a2538
Samuel Raymond <samuelraymond711@gmail.com>
2024-05-01 15:06:22 +0700

    add project picture and constants

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import gpt from "./gpt.png";
+import hoobank from "./hoobank.png";
+import metaverse from "./metaverse.png";
+import nike from "./nike.png";
+
+export { gpt, hoobank, metaverse, nike };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { nike, gpt, metaverse, hoobank } from "../../public";
+
 export const navLinks = [
   {
     id: "about",
@@ -18,5 +20,92 @@ export const navLinks = [
     link: "#projects",
     content:
       "Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it.",
+  },
+];
+
+export const projects = [
+  {
+    name: "Hoobank Website",
+    description:
+      "A modern banking website to learn about React framework, Tailwind and responsive website.",
+    tags: [
+      {
+        name: "React",
+        color: "text-react",
+      },
+      {
+        name: "Vite",
+        color: "text-vite",
+      },
+      {
+        name: "Tailwind",
+        color: "text-tailwind",
+      },
+    ],
+    image: hoobank,
+    source_code_link: "https://github.com/samuelraymond7/banking",
+    web_link: "https://samuelraymond7.github.io/banking/",
+  },
+  {
+    name: "Nike Clone",
+    description:
+      "A clone of Nike's website to learn about React framework, Tailwind and responsive website.",
+    tags: [
+      {
+        name: "React",
+        color: "text-react",
+      },
+      {
+        name: "Vite",
+        color: "text-vite",
+      },
+      {
+        name: "Tailwind",
+        color: "text-tailwind",
+      },
+    ],
+    image: nike,
+    source_code_link: "https://github.com/samuelraymond7/nike-clone",
+    web_link: "https://samuelraymond7.github.io/nike-clone/",
+  },
+  {
+    name: "GPT-3 Website",
+    description:
+      "A GPT-3 website to learn about React framework, basic CSS and responsive website.",
+    tags: [
+      {
+        name: "React",
+        color: "text-react",
+      },
+      {
+        name: "CSS",
+        color: "text-css",
+      },
+    ],
+    image: gpt,
+    source_code_link: "https://github.com/samuelraymond7/gpt",
+    web_link: "https://samuelraymond7.github.io/gpt/",
+  },
+  {
+    name: "Metaverse Website",
+    description:
+      "A Metaverse website to learn about Next.js framework, Tailwind, Framer Motion and responsive website.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "text-white",
+      },
+      {
+        name: "Tailwind",
+        color: "text-tailwind",
+      },
+      {
+        name: "Framer Motion",
+        color: "text-framer",
+      },
+    ],
+    image: metaverse,
+    source_code_link: "https://github.com/samuelraymond7/metaverse",
+    web_link: "https://samuelraymond7.github.io/metaverse/",
   },
 ];
