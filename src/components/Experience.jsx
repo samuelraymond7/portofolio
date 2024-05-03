4e3be20cf4dcfdc48116a9217f49c399e7595fa4
Samuel Raymond <samuelraymond711@gmail.com>
2024-05-03 12:28:54 +0700

    change text color in Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => (
     className="mt-[100px] w-full max-w-7xl mx-auto sm:px-16 px-6 sm:py-16 py-10"
   >
     <div>
-      <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+      <p className="sm:text-[18px] text-[14px] text-[#aaa6c3] uppercase tracking-wider">
         What I have done so far
       </p>
       <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
