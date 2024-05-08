116811274f9f49de8ba46fbeb81328f74fc11108
samuelraymond7 <159892584+samuelraymond7@users.noreply.github.com>
2024-05-08 12:52:10 +0700

    Update link in navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,20 +29,20 @@ export const navLinks = [
   {
     id: "about",
     title: "About",
-    link: "/#about",
+    link: "https://samuelraymond7.github.io/portofolio/#about",
     content:
       "I'm a front end web developer who is able to use HTML, CSS, Javascript and also TypeScript. Other than that, I can also use several frameworks such as React, Next.js and Node.js. I am a quick learner and have a high desire to learn more.",
   },
   {
     id: "work",
     title: "Work",
-    link: "/#work",
+    link: "https://samuelraymond7.github.io/portofolio/#work",
     content: "What I have done so far.",
   },
   {
     id: "projects",
     title: "Projects",
-    link: "/#projects",
+    link: "https://samuelraymond7.github.io/portofolio/#projects",
     content:
       "Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it.",
   },
